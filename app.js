8ac958704c04aa85f0bdf540fe552d65693e05cf
Unknown <lhedland0387@gmail.com>
2018-12-11 11:03:44 -0600

    first dev push
    testing git

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("my linkies stay super stinky 1k eoy!");
+	res.end("my linkies stay super stinky 1k eoy!!!");
 });
 
 server.listen(3000);
