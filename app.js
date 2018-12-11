093e0ef4c849d33867a6446aa7720702345d8fc5
PureNukage <roosevelt_stenson@yahoo.com>
2018-12-11 12:51:52 -0600

    added index.html
    updated the .js to stream the contents of index.html

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("my linkies stay super stinky 1k eoy!!!");
+	//console.log("request was made: "+req.url);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+	myReadStream.pipe(res);
 });
 
 server.listen(3000);
-
-
-
+console.log('Now listening to port 3000');
 
 /*
 var http = require('http');
