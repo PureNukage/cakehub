5525c30e6ee88d479144f13741c2cc50ea8279af
PureNukage <roosevelt_stenson@yahoo.com>
2018-12-11 14:25:48 -0600

    modifed app.js

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
+const express = require('express');
+const app = express();
+
 var http = require('http');
 var fs = require('fs');
-
+/*
 var server = http.createServer(function(req, res){
 	//console.log("request was made: "+req.url);
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
 	myReadStream.pipe(res);
 });
-
-server.listen(3000);
-console.log('Now listening to port 3000');
-
-/*
-var http = require('http');
-var fs = require('fs');
-
-var server = http.createServer(function(req, res){
-	console.log("request was made: "+req.url);
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-	myReadStream.pipe(res);
+*/
+app.get('/', (req, res) => {
+	res.send("Hello world!");
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
-*/
+const server = app.listen(3000, () => {
+	console.log(`Express running -> PORT ${server.address().port}`);
+});
